0203468ed766ead3310cba1d2c569234339d5bf3
alfilister <castillo.c.jm@gmail.com>
2022-05-11 18:30:44 -0500

    Arreglo de incompatibilidad en el review

--- api/src/controllers/review.js
+++ api/src/controllers/review.js
@@ -1,24 +1,19 @@
-<<<<<<< HEAD
-const {Review, User, Product } = require("../db")
-=======
-const { Review, User } = require("../db")
->>>>>>> 6c4f4b16dc57a6f5cfd26f12efdeb7c504cf410f
+const { Review, User, Product } = require("../db")
 
 const getReviewByProduct = async () => {
   try {
     return await Product.findAll({
-      include: Review
+      include: Review,
     })
   } catch (error) {
     console.log(error)
   }
 }
 
-<<<<<<< HEAD
 const getReviewByUser = async () => {
   try {
     return await User.findAll({
-      include: Review
+      include: Review,
     })
   } catch (error) {
     console.log(error)
@@ -30,7 +25,7 @@ const setReview = async (user_id, product_id, product_review) => {
     const review = await Review.create({
       product_review,
       product_id,
-      user_id
+      user_id,
     })
     return review
   } catch (error) {
@@ -38,11 +33,11 @@ const setReview = async (user_id, product_id, product_review) => {
   }
 }
 
-const updateReview = async (id, product_review) =>{
+const updateReview = async (id, product_review) => {
   try {
-    const review = await Review.findByPk(id);
-    review.product_review = product_review;
-    await review.save();
+    const review = await Review.findByPk(id)
+    review.product_review = product_review
+    await review.save()
     return review
   } catch (error) {
     console.log(error)
@@ -51,16 +46,17 @@ const updateReview = async (id, product_review) =>{
 
 const deleteReview = async (id) => {
   try {
-    const review = await Review.findByPk(id);
-    review.destroy();
+    const review = await Review.findByPk(id)
+    review.destroy()
   } catch (error) {
     console.log(error)
   }
 }
 
-module.exports = { getReviewByUser,  getReviewByProduct, setReview, updateReview, deleteReview}
-=======
-let test = "test"
-
-module.exports = { getReview }
->>>>>>> 6c4f4b16dc57a6f5cfd26f12efdeb7c504cf410f
+module.exports = {
+  getReviewByUser,
+  getReviewByProduct,
+  setReview,
+  updateReview,
+  deleteReview,
+}
