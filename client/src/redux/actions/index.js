e8ea354334b361465431ddd742335c8dd1cdfde2
Ferraggina <cefferra@gmail.com>
2022-05-12 15:03:25 -0300

    actualizacion de reducers

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 export const GET_PRODUCTS = "GET_PRODUCTS";
 export const GET_CATEGORIES = "GET_CATEGORIES";
+export const GET_PRODUCTS_ID = "GET_PRODUCTS_ID";
+export const GET_PRODUCTS_NAME = "GET_PRODUCTS_NAME";
 
 export function get_products() {
   return async function (dispatch) {
@@ -16,10 +18,40 @@ export function get_products() {
 
 export function getCategories() {
   return async function (dispatch) {
-    var json = await axios.get("http://localhost:3001/ap/categories");
+    var json = await axios.get("http://localhost:3001/api/categories");
     return dispatch({
       type: GET_CATEGORIES,
       payload: json.data,
     });
+  };
+}
+
+export function getProductsById(id) {
+  return async (dispatch) => {
+    try {
+      const json = await axios.get(`http://localhost:3001/api/products${id}`);
+      console.log(json.data);
+      return dispatch({
+        type: GET_PRODUCTS_ID,
+        payload: json.data,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+}
+export function getProductsByName(payload) {
+  return async function (dispatch) {
+    try {
+      const json = await axios.get(
+        `http://localhost:3001/api/products?name=${payload}`
+      );
+      return dispatch({
+        type: GET_PRODUCTS_NAME,
+        payload: json.data,
+      });
+    } catch (error) {
+      alert("No existe el producto, recarga la pagina");
+    }
   };
 }
