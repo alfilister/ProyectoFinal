9c2e4a82d03dc4c07689fc351a07b4f11d8ddbc0
Ferraggina <cefferra@gmail.com>
2022-05-13 00:07:13 -0300

    Nav estilo

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -1,32 +1,36 @@
-import React from "react"
-import { NavLink } from "react-router-dom"
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-import SearchBar from "../SearchBar"
+import SearchBar from "../SearchBar";
+import "../../scss/components/_nav.scss";
+import logo from "../../scss/assets/logo.png";
 
 const Nav = ({ setCurrentPage }) => {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-evenly",
-      }}
-    >
-      <NavLink
-        to="/"
-        style={{ textDecoration: "none", color: "black", fontWeight: "bold" }}
-      >
-        <h1>LOGO</h1>
-      </NavLink>
-      <SearchBar setCurrentPage={setCurrentPage} />
-      <NavLink to="#" style={{ textDecoration: "none", color: "black" }}>
-        <h2>inicia sesion</h2>
-      </NavLink>
-      <NavLink to="#" style={{ textDecoration: "none", color: "black" }}>
-        <h2>registrate</h2>
-      </NavLink>
+    <div className="divNavbar">
+      <div className="divSeachYLogo">
+        <NavLink
+          to="/"
+          style={{ textDecoration: "none", color: "black", fontWeight: "bold" }}
+          className="logo"
+        >
+          <h2 className="tituloPag">E-comerce Cell</h2>
+          <img className="logoImg" src={logo} alt="imagenLogo" />
+        </NavLink>
+      </div>
+      <div className="searchBarStylo">
+        <SearchBar setCurrentPage={setCurrentPage} />
+      </div>
+      <div className="textosNav">
+        <NavLink to="#" className="estiloSesion">
+          <h2>inicia sesion</h2>
+        </NavLink>
+        <NavLink to="#" className="estiloRegistro">
+          <h2>registrate</h2>
+        </NavLink>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
