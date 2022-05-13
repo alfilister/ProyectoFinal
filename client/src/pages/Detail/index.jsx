a10604dcf3d1ffe688be2525c7fc326d9390ecba
Rodrigo Orozco <orozcorodi8@gmail.com>
2022-05-12 21:16:40 -0300

    renderizado de detail basico sin reviews

--- client/src/pages/Detail/index.jsx
+++ client/src/pages/Detail/index.jsx
@@ -1,10 +1,66 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
+import Nav from "../../components/Nav";
+
+import { getProductsById } from "../../redux/actions";
 
 const Detail = () => {
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const productId = useSelector((state) => state.productsDetail);
+
+  useEffect(() => {
+    setTimeout(() => dispatch(getProductsById(id)), 3000);
+    return () => dispatch(getProductsById());
+  }, [dispatch, id]);
+
   return (
-    <div>
-      <h1>Estoy en Detail</h1>
-    </div>
+    <>
+      <Nav />
+      <div className="contenedorDetail">
+        {!productId ? (
+          <img
+            src="https://i.imgur.com/EQSYdeQ.gif"
+            alt="Loading..."
+            className="loaderDetail"
+          />
+        ) : (
+          <div className="detail">
+            <h1>{productId.name}</h1>
+            <h1>{`$ ${productId.price}`}</h1>
+            <div className="imagenDetalle">
+              <img
+                src={productId.image}
+                alt=""
+                height="300px"
+                width="500px"
+                className="imgDetalle"
+              />
+            </div>
+            <label className="subtitle">Rating:</label>
+            <p className="text">{productId.rating}</p>
+            <br />
+            <label className="subtitle">Descripción:</label>
+            <p className="text">{productId.description}</p>
+            <br />
+            <label className="subtitle">Cantidad en Stock:</label>
+            <p className="text">{productId.stock}</p>
+            <br />
+            <label className="subtitle">Descuento:</label>
+            <p className="text">
+              {productId.discount ? productId.discount : "No hay descuentos!"}
+            </p>
+            <label className="subtitle">Categorias:</label>
+            <p className="text">{productId.categories?.map((el) => el.name)}</p>
+            <br />
+            <label className="subtitle">Reviews:</label>
+            <p className="text">Debo renderizar las Reviews aún</p>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,65 +1,64 @@
-import axios from "axios"
-export const GET_PRODUCTS = "GET_PRODUCTS"
-export const GET_CATEGORIES = "GET_CATEGORIES"
-export const GET_PRODUCTS_ID = "GET_PRODUCTS_ID"
-export const GET_PRODUCTS_NAME = "GET_PRODUCTS_NAME"
+import axios from "axios";
+export const GET_PRODUCTS = "GET_PRODUCTS";
+export const GET_CATEGORIES = "GET_CATEGORIES";
+export const GET_PRODUCTS_ID = "GET_PRODUCTS_ID";
+export const GET_PRODUCTS_NAME = "GET_PRODUCTS_NAME";
 
 export function get_products() {
   return async function (dispatch) {
     try {
-      await axios.get("http://localhost:3001/api/categories")
-      await axios.get("http://localhost:3001/api/products")
+      await axios.get("http://localhost:3001/api/categories");
+      await axios.get("http://localhost:3001/api/products");
       const productsDb = await axios.get(
         "http://localhost:3001/api/products/info"
-      )
+      );
 
       return dispatch({
         type: GET_PRODUCTS,
         payload: productsDb.data.data,
-      })
+      });
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 }
 
 export function getCategories() {
   return async function (dispatch) {
-    var json = await axios.get("http://localhost:3001/api/categories")
+    var json = await axios.get("http://localhost:3001/api/categories");
     return dispatch({
       type: GET_CATEGORIES,
 
       payload: json.data,
-    })
-  }
+    });
+  };
 }
 
 export function getProductsById(id) {
   return async (dispatch) => {
     try {
-      const json = await axios.get(`http://localhost:3001/api/products${id}`)
-      console.log(json.data)
+      const json = await axios.get(`http://localhost:3001/api/products/${id}`);
       return dispatch({
         type: GET_PRODUCTS_ID,
         payload: json.data.data,
-      })
+      });
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 }
 export function getProductsByName(payload) {
   return async function (dispatch) {
     try {
       const json = await axios.get(
         `http://localhost:3001/api/products/info?name=${payload}`
-      )
+      );
       return dispatch({
         type: GET_PRODUCTS_NAME,
         payload: json.data.data,
-      })
+      });
     } catch (error) {
-      alert("No existe el producto, recarga la pagina")
+      alert("No existe el producto, recarga la pagina");
     }
-  }
+  };
 }
