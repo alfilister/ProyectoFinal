f374b00878bcfcb33b7a4df2ffdeba3508784450
Ferraggina <cefferra@gmail.com>
2022-05-13 11:47:34 -0300

    arregle el problema de la nav que al scrollear se bajaba el titulo y la search

--- client/src/components/User/profileUser.jsx
+++ client/src/components/User/profileUser.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import {useAuth0} from '@auth0/auth0-react';
+import { useAuth0 } from "@auth0/auth0-react";
 
-export default function Profile (){
-    const {user ,isAuthenticated , isLoading} = useAuth0();
+export default function Profile() {
+  const { user, isAuthenticated, isLoading } = useAuth0();
 
-    if(isLoading){
-        return <div>loading...</div>
-    }
-    return (
-        isAuthenticated && (
-          <div>
-               <img src = {user.picture} alt = {user.name} />
-               <h2> name : {user.name}</h2>
-               <p> usuario : {user.email}</p>
-          </div>
-        )
+  if (isLoading) {
+    return <div>loading...</div>;
+  }
+  return (
+    isAuthenticated && (
+      <div>
+        {/* <img src = {user.picture} alt = {user.name} /> */}
+        <h2> name : {user.name}</h2>
+        <p> usuario : {user.email}</p>
+      </div>
     )
+  );
 }
