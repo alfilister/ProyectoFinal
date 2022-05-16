683e6f8017bad4046f7cb31260c83e4f6b3a18c9
Rodrigo Orozco <orozcorodi8@gmail.com>
2022-05-16 06:49:58 -0300

    agregue el loader a la pagina 404

--- client/src/pages/Error404/index.jsx
+++ client/src/pages/Error404/index.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import loaderFantasma from "../../scss/assets/fantasma.gif";
 
 const Error404 = () => {
   return (
     <div className="body">
-      <h1>ERROR 404</h1>
-      <p>Lo sentimos mucho colega, pero esta pagina no existe.</p>
-      <p>
-        podes volver a la <Link to="/">HOME</Link> con ese enlace 😉
-      </p>
+      <div className="mensaje">
+        <h1>ERROR 404</h1>
+        <p>Lo sentimos mucho colega, pero esta pagina no existe.</p>
+        <p>
+          podes volver a la <Link to="/">HOME</Link> con ese enlace 😉
+        </p>
+      </div>
+      <div className="loader">
+        <img src={loaderFantasma} alt="" />
+      </div>
     </div>
   );
 };
