5fb5077d9fa6d09c4590f66a7342525a09dc7e35
Ferraggina <cefferra@gmail.com>
2022-05-18 18:03:41 -0300

    redux review y validaciones

---
+++ client/src/components/createReview/renderReviewCreate/index.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+import StarRating from "../startRating/index";
+import ChangeRating from "../changeRating/index";
+import { postReview } from "../../../redux/actions";
+
+export default function Rating() {
+  // Rating
+  const [avgRating, setAvgRating] = useState(0);
+
+  const handleRating = (input) => {
+    setAvgRating(input);
+  };
+
+  return (
+    <div className="App">
+      <h1>Star Rating</h1>
+      <ChangeRating rating={avgRating} handleRating={handleRating} />
+      <br />
+      <br />
+      <StarRating stars={avgRating} />
+    </div>
+  );
+}
