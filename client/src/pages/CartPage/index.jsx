9120734fba43fd3db205f1d1a89f51ca7ff765da
alfilister <castillo.c.jm@gmail.com>
2022-05-18 00:21:53 -0500

    Cart Structure Done

--- client/src/pages/CartPage/index.jsx
+++ client/src/pages/CartPage/index.jsx
@@ -12,29 +12,36 @@ function CartPage() {
   var finalValue = subTotal[0] && subTotal.reduce((a, c) => a + c).toFixed(2)
 
   const [total, setTotal] = useState(finalValue)
+  var taxIva = (Number(total) * 0.19).toFixed(2)
+  var final = (Number(total) + Number(taxIva)).toFixed(2)
 
   return (
-    <div className="cartPage">
+    <div>
       <Nav />
-      <div className="itemsInCart">
-        {initialCart?.map(
-          (el) =>
-            el.quantity > 0 && (
-              <CartProduct
-                key={el.id}
-                id={el.id}
-                quantity={el.quantity}
-                product={el.product}
-                setTotal={setTotal}
-                total={total}
-              />
-            )
-        )}
-      </div>
+      <div className="cartPage">
+        <div className="itemsInCart">
+          {initialCart?.map(
+            (el) =>
+              el.quantity > 0 && (
+                <CartProduct
+                  key={el.id}
+                  id={el.id}
+                  quantity={el.quantity}
+                  product={el.product}
+                  setTotal={setTotal}
+                  total={total}
+                />
+              )
+          )}
+        </div>
 
-      <div className="totalAmount">
-        <h3>Total Purchase</h3>
-        <h3>{total}</h3>
+        <div className="totalAmount">
+          <div className="line"></div>
+          <h3>SubTotal $ {total}</h3>
+          <h3>Taxes $ {taxIva}</h3>
+          <h2>Total $ {final}</h2>
+          <button>Proceed to checkout</button>
+        </div>
       </div>
     </div>
   )
