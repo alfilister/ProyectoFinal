3fd6a0cfbbeb08c3ba74ca019fab19c2a4d8a87a
Rodrigo Orozco <orozcorodi8@gmail.com>
2022-05-18 12:30:13 -0300

    pusheando a la dev-branch

--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -1,42 +1,42 @@
-const { Router } = require("express")
-const userController = require("../controllers/user")
-const { User } = require("../db")
+const { Router } = require("express");
+const userController = require("../controllers/user");
+const { User } = require("../db");
 
-const router = Router()
+const router = Router();
 
 router.get("/", async (req, res, next) => {
   try {
     res.json({
       status: "Users loaded",
       results: await userController.getUser(),
-    })
+    });
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
 router.get("/:idUser", async (req, res, next) => {
-  const {idUser} = req.params
+  const { idUser } = req.params;
   try {
     res.json({
       status: "Users loaded",
       results: await userController.getUserById(idUser),
-    })
+    });
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
 router.post("/", async (req, res, next) => {
-  const { user } = req.body
+  const { user } = req.body;
   try {
     res.json({
       status: "Users created",
-      results: await userController.postUser( user ),
-    })
+      results: await userController.postUser(user),
+    });
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
-module.exports = router
+module.exports = router;
