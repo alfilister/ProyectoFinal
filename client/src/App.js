bcb24ac93ceb545295d2e3aaf399cae51a2eec53
Ferraggina <cefferra@gmail.com>
2022-05-19 01:19:42 -0300

    Componente terminado

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,28 @@
-import "./_app.scss"
-import React, { useEffect, useState } from "react"
-import { Routes, Route } from "react-router-dom"
-import { useDispatch, useSelector } from "react-redux"
+import "./_app.scss";
+import React, { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
-import Home from "./pages/Home"
-import Detail from "./pages/Detail"
-import Error404 from "./pages/Error404"
-import Category from "./pages/Category"
-import ControlPanel from "./pages/Admin"
-import CartPage from "./pages/CartPage"
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
+import Error404 from "./pages/Error404";
+import Category from "./pages/Category";
+import ControlPanel from "./pages/Admin";
+import CartPage from "./pages/CartPage";
+import ProductReview from "./components/createReview/renderReviewCreate/index";
 
-import { getProducts, getCategories, getReviewsProduct } from "./redux/actions"
-import Nav from "./components/Nav"
+import { getProducts, getCategories, getReviewsProduct } from "./redux/actions";
+import Nav from "./components/Nav";
 
 function App() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getCategories())
-    setTimeout(() => dispatch(getProducts()), 1000)
-    dispatch(getProducts())
-    dispatch(getReviewsProduct())
-  }, [dispatch])
+    dispatch(getCategories());
+    setTimeout(() => dispatch(getProducts()), 1000);
+    dispatch(getProducts());
+    dispatch(getReviewsProduct());
+  }, [dispatch]);
 
   return (
     <div>
@@ -33,9 +34,10 @@ function App() {
         <Route path="/category/:categoryName" element={<Category />} />
         <Route path="/controlPanel" element={<ControlPanel />} />
         <Route path="/cart" element={<CartPage />} />
+        <Route path="/reviewsPost" element={<ProductReview />} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/components/createReview/renderReviewCreate/index.jsx
+++ client/src/components/createReview/renderReviewCreate/index.jsx
@@ -1,25 +1,72 @@
 import React, { useState } from "react";
 
+import { useDispatch } from "react-redux";
 import StarRating from "../startRating/index";
-import ChangeRating from "../changeRating/index";
+// import ChangeRating from "../changeRating/index";
 import { postReview } from "../../../redux/actions";
 
 export default function Rating() {
-  // Rating esto es un cambio para subir a la dev branch
+  const dispatch = useDispatch();
 
-  const [avgRating, setAvgRating] = useState(0);
+  // const [avgRating, setAvgRating] = useState({ score_review: 0 });
+  const [input, setInput] = useState({
+    product_review: "",
+    score_review: 0,
+  });
 
-  const handleRating = (input) => {
-    setAvgRating(input);
-  };
+  function handleChangeInputReview(e) {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+
+    console.log(input);
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    // dispatch(postReview(avgRating));
+    dispatch(postReview(input));
+
+    setInput({
+      product_review: "",
+      score_review: 0,
+    });
+    // setAvgRating({ score_review: 0 });
+
+    alert("Reseña publicada");
+  }
+
+  // const handleRating = (input) => {
+  //   setAvgRating(input);
+  //   console.log(input);
+  // };
 
   return (
     <div className="App">
       <h1>Star Rating</h1>
-      <ChangeRating rating={avgRating} handleRating={handleRating} />
-      <br />
-      <br />
-      <StarRating stars={avgRating} />
+      {/* <ChangeRating rating={avgRating} handleRating={handleRating} /> */}
+      <StarRating stars={input.score_review} />
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <label>Escribir Opinion:</label>
+        <input
+          type="text"
+          value={input.product_review}
+          name="product_review"
+          onChange={handleChangeInputReview}
+        />
+        <input
+          type="number"
+          step="0.5"
+          min="0"
+          max="5"
+          value={input.score_review}
+          name="score_review"
+          onChange={handleChangeInputReview}
+        />
+
+        <button type="submit"> Publicar reseña </button>
+      </form>
     </div>
   );
 }
