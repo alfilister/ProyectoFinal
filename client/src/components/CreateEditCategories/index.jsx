4ddf37800bd6c82278c48ed37f67ced107ac7a95
kamo2312 <cristianctr97@gmail.com>
2022-05-29 01:54:45 -0500

    bugs and editCategories

--- client/src/components/CreateEditCategories/index.jsx
+++ client/src/components/CreateEditCategories/index.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Modal, ModalBody, ModalFooter } from "reactstrap";
-import { createCategory } from "../../redux/actions";
+import { createCategory, editCategory } from "../../redux/actions";
 
 function CreateEditCategories() {
 	const dispatch = useDispatch();
@@ -10,6 +10,11 @@ function CreateEditCategories() {
 	const [newCateogry, setNewCategory] = useState(false);
 	const [nameCategory, setNameCategory] = useState({ nameCategory: "" });
 	const [showCategories, setShowCategories] = useState(allCategories);
+	const [editModal, setEditModal] = useState(false);
+	const [categorySelected, setCategorySelected] = useState({
+		id: "",
+		name: "",
+	});
 
 	showCategories.sort((a, b) => {
 		if (a.name.toLowerCase() > b.name.toLowerCase()) {
@@ -40,6 +45,33 @@ function CreateEditCategories() {
 		}
 	};
 
+	const editCategorylocal = (category) => {
+		console.log(category);
+		setCategorySelected(category);
+		setEditModal(true);
+	};
+
+	const handleEdit = (event) => {
+		const { value } = event.target;
+		setCategorySelected({
+			...categorySelected,
+			name: value,
+		});
+	};
+
+	const changeCategory = () => {
+		dispatch(editCategory(categorySelected));
+		setEditModal(false);
+		const newData = showCategories;
+		newData.map((c) => {
+			if (c.id === categorySelected.id) {
+				c.name = categorySelected.name;
+			}
+			return newData;
+		});
+		setShowCategories(newData);
+	};
+
 	return (
 		<div>
 			<button
@@ -53,6 +85,7 @@ function CreateEditCategories() {
 				<thead>
 					<tr>
 						<td>Nombre</td>
+						<td>Editar</td>
 					</tr>
 				</thead>
 				<tbody>
@@ -60,6 +93,15 @@ function CreateEditCategories() {
 						return (
 							<tr>
 								<td>{category.name}</td>
+								<td>
+									<button
+										onClick={() => {
+											editCategorylocal(category);
+										}}
+									>
+										🖊
+									</button>
+								</td>
 							</tr>
 						);
 					})}
@@ -79,6 +121,23 @@ function CreateEditCategories() {
 				<ModalFooter>
 					<button onClick={() => createCat()}>Crear</button>
 					<button onClick={() => setNewCategory(false)}>Cancelar</button>
+				</ModalFooter>
+			</Modal>
+
+			<Modal isOpen={editModal}>
+				<ModalBody>
+					<label>Nuevo nombre:</label>
+					<input
+						type="text"
+						value={categorySelected.name}
+						onChange={(event) => {
+							handleEdit(event);
+						}}
+					></input>
+				</ModalBody>
+				<ModalFooter>
+					<button onClick={() => changeCategory()}>Crear</button>
+					<button onClick={() => setEditModal(false)}>Cancelar</button>
 				</ModalFooter>
 			</Modal>
 		</div>
