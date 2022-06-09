5b990454c441db6cabdbed1584e17fca8f5b7058
Jorge Castillo <castillo.c.jm@gmail.com>
2022-06-09 18:24:30 -0500

    Update index.jsx

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,16 +1,11 @@
 import React from "react";
-import Nav from "../../components/Nav";
 import CardSlider from "../../components/CardSlider";
-import CategoryGrid from "../../components/CategoryGrid";
 import PersonalizedSuggest from "../../components/PersonalizedSuggest";
-import { useSelector } from "react-redux";
 
 const Home = () => {
   return (
     <div className="home">
-
       <div className="bannerHome"></div>
-
       <PersonalizedSuggest />
       <CardSlider />
     </div>
