ddb446dde9bdda3dc7687ef12a5a8317d763c8d8
Jorge Castillo <castillo.c.jm@gmail.com>
2022-06-09 22:36:50 -0500

    Update Category.js

--- api/src/models/Category.js
+++ api/src/models/Category.js
@@ -1,13 +1,68 @@
-const { DataTypes } = require("sequelize")
+const { Router } = require("express");
+const categoryController = require("../controllers/category");
 
-module.exports = (sequelize) => {
-  sequelize.define(
-    "category",
-    {
-      name: {
-        type: DataTypes.STRING,
-      },
-    },
-    { timestamps: false, createdAt: false, updatedAt: false }
-  )
-}
+const router = Router();
+
+// RUTA PARA CARGAR Y OBTENER LAS CATEGORÍAS
+router.get("/", async (req, res, next) => {
+	try {
+		const categories = await categoryController.getCategories();
+
+		res.json({
+			status: "Api info loaded",
+			results: categories,
+		});
+	} catch (err) {
+		next(err);
+	}
+});
+
+// RUTA PARA CREAR CATEGORÍAS
+router.post("/", async (req, res, next) => {
+	try {
+		const { nameCategory } = req.body;
+
+		const categories = await categoryController.postCategory(nameCategory);
+
+		res.json({
+			status: "Category created",
+			results: categories,
+		});
+	} catch (err) {
+		next(err);
+	}
+});
+
+//RUTA PARA BORRAR CATEGORÍAS
+router.delete("/", async (req, res, next) => {
+	try {
+		const { idCategory } = req.body;
+
+		const categories = await categoryController.deleteCategory(idCategory);
+
+		res.json({
+			status: "Category deleted",
+			results: categories,
+		});
+	} catch (err) {
+		next(err);
+	}
+});
+
+// RUTA PARA ACTUALIZAR CATEGORÍAS
+router.put("/", async (req, res, next) => {
+	console.log(req.body);
+	try {
+		const { id, name } = req.body;
+		const categories = await categoryController.updateCategory(id, name);
+
+		res.json({
+			status: "Category updated",
+			results: categories,
+		});
+	} catch (err) {
+		next(err);
+	}
+});
+
+module.exports = router;
