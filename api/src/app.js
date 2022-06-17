87d59b82613a4f0fd190c486dda8d4ebee209f12
Rodrigo Orozco <orozcorodi8@gmail.com>
2022-06-17 20:46:00 -0300

    cambios cors para deploy backend

--- api/src/app.js
+++ api/src/app.js
@@ -11,26 +11,27 @@ const cors = require("cors");
 
 server.name = "API";
 
-server.use(
-  cors({
-    origin: "*",
-    credentials: true,
-  })
-);
+// server.use(
+//   cors({
+//     origin: "*",
+//     credentials: true,
+//   })
+// );
+server.use(cors())
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
-// server.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Credentials", "true");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-//   next();
-// });
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 
 server.use("/api", routes);
 
